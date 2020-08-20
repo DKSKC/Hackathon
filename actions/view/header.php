5b709bf786281bef689ed2280fed437baad17294
Ke'an Consolacion <keanconsolacion@gmail.com>
2020-08-20 17:56:36 +0800

    added actions

---
+++ actions/view/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="../assets/vendor/bootstrap/css/bootstrap.min.css"
+    />
+    <link href="../assets/vendor/fonts/circular-std/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/libs/css/style.css" />
+    <link
+      rel="stylesheet"
+      href="../assets/vendor/fonts/fontawesome/css/fontawesome-all.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../assets/vendor/charts/chartist-bundle/chartist.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../assets/vendor/charts/morris-bundle/morris.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css"
+    />
+    <link rel="stylesheet" href="../assets/vendor/charts/c3charts/c3.css" />
+    <link
+      rel="stylesheet"
+      href="../assets/vendor/fonts/flag-icon-css/flag-icon.min.css"
+    />
+    
+    <style>
+        @media only screen and (max-width: 600px) {
+            body {
+                padding: 20px;
+                font-size: 10px;
+            }
+        }
+    </style>
+    
+    <title>Insert/Update Project</title>
+  </head>
+  <body style='background-color:#EFEFF6'>
